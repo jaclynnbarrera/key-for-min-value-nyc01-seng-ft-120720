acf1f7e85af5a0679ddeaccce3026d1d34a07292
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-11-26 17:27:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,14 +1,6 @@
 
 def key_for_min_value(name_hash)
   
-  min_key = nil
-  min_value = 1000
   
-  name_hash.each do |key, value|
-  if min_value > value
-    min_value = value
-    min_key = key
-  end
-end
-  p min_key
+  
 end
